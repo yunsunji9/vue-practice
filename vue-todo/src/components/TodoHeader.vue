04b1f2fd1d7d695eee36851f82ad01d211a55f72
seonji <yunsunji9@hanmail.net>
2021-02-15 00:47:02 +0900

    vue-todo 프로젝트 완료 토글버튼까지 구현

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1>Todo Header</h1>
+    </header>
+</template>
+
+<style scoped>
+h1 {
+    color: #2f3b52;
+    font-weight: 600;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
