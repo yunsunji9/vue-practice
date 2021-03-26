526b553e9682e31e2c4d40d12de377d11dab753a
yunsunji9 <yunsunji9@hanmail.net>
2021-03-26 09:20:00 +0900

    chart.js 사용하기

---
+++ vue-chart/chart-lib/src/components/BarChart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'propsdata'
+  ],
+  mounted() {
+    new this.$_Chart(this.$refs.barChart, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: this.propsdata
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-chart/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <canvas ref="myLineChart" id="myLineChart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    new this.$_Chart(this.$refs.myLineChart.getContext('2d'), {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
